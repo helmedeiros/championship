22162ad3b7fc23555d851a63b77870a3cbb7719f
helmedeiros <helio.cabralmedeiros@gmail.com>
2014-08-12 18:10:38 +0200

    chore(deploy): publicar build apos 3e96ba3505a2fcbb20bc912e013e146bb6457b8b

--- src/models/match_event.js
+++ src/models/match_event.js
@@ -4,7 +4,33 @@ module.exports = (function () {
   var Backbone = require('backbone');
   var messages = require('./messages/match_event');
 
+  var TYPES = [
+    'kickoff',
+    'goal',
+    'own_goal',
+    'yellow',
+    'red',
+    'sub',
+    'var',
+    'comment',
+    'half_time',
+    'full_time'
+  ];
+
+  var HALVES = [1, 2, 3, 4];
+
+  function contains(list, value) {
+    var i;
+    for (i = 0; i < list.length; i = i + 1) {
+      if (list[i] === value) { return true; }
+    }
+    return false;
+  }
+
   return Backbone.Model.extend({
+
+    TYPES: TYPES,
+    HALVES: HALVES,
 
     defaults: {
       type: '',
@@ -17,6 +43,15 @@ module.exports = (function () {
     validate: function (attrs) {
       if (!attrs.type) {
         return messages.TYPE_REQUIRED;
+      }
+      if (!contains(TYPES, attrs.type)) {
+        return messages.TYPE_UNKNOWN;
+      }
+      if (!contains(HALVES, attrs.half)) {
+        return messages.HALF_UNKNOWN;
+      }
+      if (typeof attrs.minute !== 'number' || attrs.minute < 0 || attrs.minute > 120) {
+        return messages.MINUTE_RANGE;
       }
     }
 
