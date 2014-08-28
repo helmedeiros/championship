ed3b2153710e4364b3f4e0c7968ed4a8d0e12836
helmedeiros <helio.cabralmedeiros@gmail.com>
2014-08-28 15:43:25 +0200

    feat(times): criar TeamRowView para renderizar linha de time

---
+++ test/spec/views/teams/row_view_spec.js
@@ -0,0 +1,43 @@
+'use strict';
+
+require('../../../helpers/dom')({ reset: true });
+
+var Team = require('../../../../src/models/team');
+var TeamRowView = require('../../../../src/views/teams/row_view');
+
+describe('views/teams/RowView', function () {
+
+  it('renderiza uma linha de tabela para um time', function () {
+    var view = new TeamRowView({
+      model: new Team({
+        name: 'Santos',
+        short: 'SAN',
+        city: 'Santos',
+        stadium: 'Vila Belmiro'
+      })
+    });
+    view.render();
+    expect(view.el.tagName.toLowerCase()).to.equal('tr');
+    expect(view.$('.team-name').text()).to.equal('Santos');
+    expect(view.$('.team-short').text()).to.equal('SAN');
+    expect(view.$('.team-city').text()).to.equal('Santos');
+    expect(view.$('.team-stadium').text()).to.equal('Vila Belmiro');
+  });
+
+  it('escapa caracteres estruturais de HTML para evitar XSS', function () {
+    var view = new TeamRowView({
+      model: new Team({ name: '<script>alert(1)</script>' })
+    });
+    view.render();
+    expect(view.$('.team-name script').length).to.equal(0);
+    expect(view.$('.team-name').text()).to.equal('<script>alert(1)</script>');
+  });
+
+  it('renderiza colunas vazias quando o time não tem cidade ou estádio', function () {
+    var view = new TeamRowView({ model: new Team({ name: 'Sem Casa' }) });
+    view.render();
+    expect(view.$('.team-city').text()).to.equal('');
+    expect(view.$('.team-stadium').text()).to.equal('');
+  });
+
+});
