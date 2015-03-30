8991106e5f0227fc7f60ec9594e89004c5ce1b64
helmedeiros <helio.cabralmedeiros@gmail.com>
2015-03-30 11:53:26 +0200

    feat(partida): linkar times e adicionar atalho h2h no header

--- src/views/matches/header_view.js
+++ src/views/matches/header_view.js
@@ -3,6 +3,7 @@ module.exports = (function () {
 
   var Marionette = require('backbone.marionette');
   var escapeHtml = require('../helpers/escape_html');
+  var teamLink = require('../helpers/team_link');
   var STATUS_LABEL = require('./status_labels').LONG;
 
   return Marionette.ItemView.extend({
@@ -15,14 +16,17 @@ module.exports = (function () {
           escapeHtml(data.stadium || '—') +
         '</div>' +
         '<div class="match-scoreboard">' +
-          '<span class="team-home">' + escapeHtml(data.home) + '</span>' +
+          '<span class="team-home">' + teamLink.link(data.home) + '</span>' +
           '<span class="score-home">' + escapeHtml(data.homeScore) + '</span>' +
           '<span class="vs">×</span>' +
           '<span class="score-away">' + escapeHtml(data.awayScore) + '</span>' +
-          '<span class="team-away">' + escapeHtml(data.away) + '</span>' +
+          '<span class="team-away">' + teamLink.link(data.away) + '</span>' +
         '</div>' +
         '<div class="match-status status-' + data.status + '">' +
           escapeHtml(STATUS_LABEL[data.status] || data.status) +
+        '</div>' +
+        '<div class="match-h2h-link text-center">' +
+          teamLink.h2hLink(data.home, data.away, 'ver histórico do confronto') +
         '</div>';
     },
 
