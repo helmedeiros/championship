a99b2afe7f418488dacc3e59c64144ac68996709
helmedeiros <helio.cabralmedeiros@gmail.com>
2015-06-30 17:37:02 +0200

    chore(deploy): publicar build apos 6a58f9ddc18615ef25e1bfd69bdfa289650867dd

--- bundle.js
+++ bundle.js
@@ -25943,17 +25943,19 @@ module.exports = (function () {
   'use strict';
 
   var Marionette = require('backbone.marionette');
+  var i18n = require('../../i18n');
 
   return Marionette.ItemView.extend({
     tagName: 'tr',
     className: 'matches-empty',
     template: function () {
-      return '<td colspan="6" class="text-muted">Nenhuma partida agendada.</td>';
+      return '<td colspan="6" class="text-muted" role="status">' +
+        i18n.t('common.empty') + '</td>';
     }
   });
 }());
 
-},{"backbone.marionette":37}],155:[function(require,module,exports){
+},{"../../i18n":108,"backbone.marionette":37}],155:[function(require,module,exports){
 module.exports = (function () {
   'use strict';
 
