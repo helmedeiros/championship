6a58f9ddc18615ef25e1bfd69bdfa289650867dd
helmedeiros <helio.cabralmedeiros@gmail.com>
2015-06-30 17:33:35 +0200

    feat(a11y): empty state com role=status e i18n na lista de partidas

--- src/views/matches/empty_view.js
+++ src/views/matches/empty_view.js
@@ -2,12 +2,14 @@ module.exports = (function () {
   'use strict';
 
   var Marionette = require('backbone.marionette');
+  var i18n = require('../../i18n');
 
   return Marionette.ItemView.extend({
     tagName: 'tr',
     className: 'matches-empty',
     template: function () {
-      return '<td colspan="6" class="text-muted">Nenhuma partida agendada.</td>';
+      return '<td colspan="6" class="text-muted" role="status">' +
+        i18n.t('common.empty') + '</td>';
     }
   });
 }());
