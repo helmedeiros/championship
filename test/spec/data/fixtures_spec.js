8a36fd5c181f12878344cecc64a910ae164e6213
helmedeiros <helio.cabralmedeiros@gmail.com>
2015-07-21 13:47:08 +0200

    test(data): validar fixture da Copa América 2015

--- test/spec/data/fixtures_spec.js
+++ test/spec/data/fixtures_spec.js
@@ -3,6 +3,7 @@
 var validator = require('../../../src/importer/validator');
 var worldCup = require('../../../src/data/world_cup_2014');
 var brasileirao = require('../../../src/data/brasileirao_2014');
+var copaAmerica = require('../../../src/data/copa_america_2015');
 
 describe('data/world_cup_2014 fixture', function () {
 
@@ -59,6 +60,36 @@ describe('data/brasileirao_2014 fixture', function () {
     var ids = {};
     brasileirao.teams.forEach(function (t) { ids[t.id] = true; });
     brasileirao.matches.forEach(function (m) {
+      expect(ids[m.home], 'home ' + m.home).to.equal(true);
+      expect(ids[m.away], 'away ' + m.away).to.equal(true);
+    });
+  });
+
+});
+
+describe('data/copa_america_2015 fixture', function () {
+
+  it('passa pelo validator (schema válido)', function () {
+    var result = validator.validate(copaAmerica);
+    if (!result.valid) {
+      throw new Error('Erros: ' + JSON.stringify(result.errors));
+    }
+    expect(result.valid).to.equal(true);
+  });
+
+  it('tem 12 seleções', function () {
+    expect(copaAmerica.teams).to.have.length(12);
+  });
+
+  it('tem 26 partidas (18 grupos + 4 quartas + 2 semi + 2 finais)',
+    function () {
+      expect(copaAmerica.matches).to.have.length(26);
+    });
+
+  it('todos os times referenciados existem', function () {
+    var ids = {};
+    copaAmerica.teams.forEach(function (t) { ids[t.id] = true; });
+    copaAmerica.matches.forEach(function (m) {
       expect(ids[m.home], 'home ' + m.home).to.equal(true);
       expect(ids[m.away], 'away ' + m.away).to.equal(true);
     });
