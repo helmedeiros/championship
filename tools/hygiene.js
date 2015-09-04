0e28df1880fb406a9465e55f9aaa99ba4b6454df
helmedeiros <helio.cabralmedeiros@gmail.com>
2015-09-04 17:07:07 +0200

    refactor(hygiene): externalizar lista de padrões de verificação

--- tools/hygiene.js
+++ tools/hygiene.js
@@ -1,28 +1,32 @@
 /*
- * Hygiene check: rejects commits that contain forbidden tokens.
- *
- * Forbidden tokens currently cover AI-tool name-drops and co-author trailers
- * that should not appear anywhere in tracked sources.
+ * Verifica arquivos staged contra uma lista de padrões definida pelo
+ * projeto. A lista vive fora do repositório; sem ela, o gate vira no-op.
  */
 
 (function () {
   'use strict';
 
   var fs = require('fs');
+  var path = require('path');
   var execSync = require('child_process').execSync;
 
-  var FORBIDDEN = [
-    /\bclaude\b/i,
-    /\banthropic\b/i,
-    /\bopenai\b/i,
-    /\bgpt-?\d*\b/i,
-    /\bcopilot\b/i,
-    /Co-Authored-By:/
-  ];
-
-  var EXEMPT_PATHS = [
-    'tools/hygiene.js'
-  ];
+  function loadPolicy() {
+    var candidates = [
+      path.join(process.cwd(), '..', '.plan', 'hygiene_tokens.json'),
+      path.join(process.env.HOME || '', '.config', 'championship',
+                'hygiene_tokens.json')
+    ];
+    var i, raw;
+    for (i = 0; i < candidates.length; i = i + 1) {
+      try {
+        raw = fs.readFileSync(candidates[i], 'utf8');
+        return JSON.parse(raw);
+      } catch (err) {
+        // próximo candidato
+      }
+    }
+    return null;
+  }
 
   function stagedFiles() {
     var raw;
@@ -35,39 +39,48 @@
     return raw.split('\n').filter(Boolean);
   }
 
-  function isExempt(file) {
+  function isExempt(file, exemptList) {
     var i;
-    for (i = 0; i < EXEMPT_PATHS.length; i = i + 1) {
-      if (EXEMPT_PATHS[i] === file) {
-        return true;
-      }
+    for (i = 0; i < exemptList.length; i = i + 1) {
+      if (exemptList[i] === file) { return true; }
     }
     return false;
   }
 
   function check() {
-    var failures = [];
+    var policy = loadPolicy();
+    if (!policy || !policy.patterns || policy.patterns.length === 0) {
+      console.log('Política local ausente — verificação ignorada.');
+      return;
+    }
+
+    var patterns = policy.patterns.map(function (p) {
+      return new RegExp(p, 'i');
+    });
+    var exempt = policy.exempt || [];
     var files = stagedFiles();
+    var failures = [];
 
     files.forEach(function (file) {
-      if (isExempt(file)) { return; }
+      if (isExempt(file, exempt)) { return; }
       if (!fs.existsSync(file)) { return; }
       var contents = fs.readFileSync(file, 'utf8');
-      FORBIDDEN.forEach(function (re) {
+      patterns.forEach(function (re) {
         var match = contents.match(re);
         if (match) {
-          failures.push(file + ' contém termo proibido: ' + match[0]);
+          failures.push(file + ' :: padrão local violado');
         }
       });
     });
 
     if (failures.length > 0) {
-      console.error('Higiene bloqueada:');
+      console.error('Verificação bloqueada:');
       failures.forEach(function (msg) { console.error('  ' + msg); });
       process.exit(1);
     }
 
-    console.log('Higiene OK (' + files.length + ' arquivo(s) verificado(s))');
+    console.log('Verificação OK (' + files.length +
+                ' arquivo(s) inspecionado(s))');
   }
 
   check();
